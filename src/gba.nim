f7082b6d710808f1fd687d75c725e8369869686c
Matthew Berry <me@mattrb.com>
2021-02-16 21:27:32 -0800

    running in emscripten

--- src/gba.nim
+++ src/gba.nim
@@ -3,9 +3,6 @@ import sdl2
 
 import gba/[bus, cpu, display, ppu, scheduler, types]
 
-if paramCount() != 2:
-  quit "Run with ./gba /path/to/bios /path/to/rom"
-
 proc newGBA(bios, rom: string): GBA =
   new result
   result.scheduler = newScheduler()
@@ -14,11 +11,25 @@ proc newGBA(bios, rom: string): GBA =
   result.cpu = newCPU(result)
   result.ppu = newPPU(result)
 
+proc runFrame(gba: GBA) =
+  for _ in 0 ..< 280896:
+    gba.cpu.tick()
+    gba.scheduler.tick(1)
+
+proc loop(gba: GBA) {.cdecl.} =
+  runFrame(gba)
+
 discard sdl2.init(INIT_EVERYTHING)
 
-var
-  gba = newGBA(paramStr(1), paramStr(2))
-
-while true:
-  gba.cpu.tick()
-  gba.scheduler.tick(1)
+when defined(emscripten):
+  type em_arg_callback_func = proc(data: pointer) {.cdecl.}
+  proc emscripten_set_main_loop(fun: proc() {.cdecl.}, fps, simulate_infinite_loop: cint) {.header: "<emscripten.h>".}
+  proc emscripten_set_main_loop_arg(fun: em_arg_callback_func, arg: pointer, fps, simulate_infinite_loop: cint) {.header: "<emscripten.h>".}
+  proc emscripten_cancel_main_loop() {.header: "<emscripten.h>".}
+  proc initFromEmscripten() {.exportc.} =
+    var gba = newGBA("bios.bin", "rom.gba")
+    emscripten_set_main_loop_arg(cast[em_arg_callback_func](loop), cast[pointer](gba), -1, 1)
+else:
+  if paramCount() != 2: quit "Run with ./gba /path/to/bios /path/to/rom"
+  var gba = newGBA(paramStr(1), paramStr(2))
+  while true: loop(gba)
