a20edf178b66b97812de2bea0faef70c8d42f9db
Matthew Berry <me@mattrb.com>
2021-02-18 22:22:25 -0800

    functional dpad

--- web/index.js
+++ web/index.js
@@ -26,17 +26,11 @@ const readToEmscriptenFileSystem = (filename, filter = "") => {
     });
 };
 
-const pressButton = keyCode => {
-    let eventObj = document.createEventObject ? document.createEventObject() : document.createEvent("Events");
-
-    if (eventObj.initEvent) {
-        eventObj.initEvent("keydown", true, true);
-    }
-
-    eventObj.keyCode = keyCode;
-    eventObj.which = keyCode;
-
-    document.dispatchEvent ? document.dispatchEvent(eventObj) : document.fireEvent("onkeydown", eventObj);
+const pressButton = (keyCode, down = true) => {
+    let event = new Event(down ? "keydown" : "keyup", { "bubbles": true, cancelable: "true" })
+    event.keyCode = keyCode;
+    event.which = keyCode;
+    document.dispatchEvent(event);
 }
 
 const reportFps = fps => {
@@ -53,6 +47,43 @@ document.getElementById("open-rom").addEventListener("click",
 
 document.getElementById("select").addEventListener("click",
     () => pressButton(Button.SELECT));
+
+var currentDpad = null;
+
+document.getElementById("dpad").addEventListener("touchstart", event => {
+    let element = event.target;
+    if (currentDpad == null && element.hasAttribute("button")) {
+        let keyCode = element.getAttribute("button");
+        pressButton(keyCode, true);
+        currentDpad = keyCode;
+    }
+});
+
+document.getElementById("dpad").addEventListener("touchend", event => {
+    let touch = event.changedTouches[0];
+    let element = document.elementFromPoint(touch.clientX, touch.clientY);
+    if (element.hasAttribute("button")) {
+        pressButton(element.getAttribute("button"), false);
+        currentDpad = null;
+    }
+});
+
+document.getElementById("dpad").addEventListener("touchmove", event => {
+    event.preventDefault();
+    let touch = event.touches[0];
+    let element = document.elementFromPoint(touch.clientX, touch.clientY);
+    if (element.hasAttribute("button")) {
+        let keyCode = element.getAttribute("button");
+        if (keyCode != currentDpad) {
+            pressButton(currentDpad, false);
+            pressButton(keyCode, true);
+            currentDpad = keyCode;
+        }
+    } else if (currentDpad != null) {
+        pressButton(currentDpad, false);
+        currentDpad = null;
+    }
+});
 
 var Module = {
     print: (() => {
