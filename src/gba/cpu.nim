327ff5bcdc7ff242a7e2b7c73443d7813d1165ef
Matthew Berry <me@mattrb.com>
2021-03-21 12:29:15 -0700

    fix minor addition overflow flag set

--- src/gba/cpu.nim
+++ src/gba/cpu.nim
@@ -97,7 +97,7 @@ proc add*(cpu: var CPU, op1, op2: uint32, setCond: bool): uint32 =
   if setCond:
     setNegAndZeroFlags(cpu, result)
     cpu.cpsr.carry = result < op1
-    cpu.cpsr.overflow = not((op1 xor op2) and (op2 xor result)).testBit(31)
+    cpu.cpsr.overflow = (not(op1 xor op2) and (op2 xor result)).testBit(31)
 
 proc sub*(cpu: var CPU, op1, op2: uint32, setCond: bool): uint32 =
   result = op1 - op2
