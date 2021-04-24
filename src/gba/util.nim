992b87ad5ef5a2def994c482d143205b6b446cc1
Matthew Berry <me@mattrb.com>
2021-04-24 14:41:47 -0700

    add wrappers to read and write arbitrary values to arrays

--- src/gba/util.nim
+++ src/gba/util.nim
@@ -15,3 +15,14 @@ func bit*(value, bit: SomeUnsignedInt): bool {.inline.} = bool((value >> bit) an
 func signExtend*(T: typedesc, value, bit: SomeUnsignedInt): T =
   result = cast[T](value)
   if value.bit(bit): result = result or (high(T) shl bit)
+
+# pointer arithmetic
+func `+`[T](p: ptr T, offset: int): ptr T {.inline.} = cast[ptr T](cast[int](p) + offset * sizeof(T))
+func `[]`[T](p: ptr T, offset: int): T {.inline.} = (p + offset)[]
+func `[]=`[T](p: ptr T, offset: int, val: T) {.inline.} = (p + offset)[] = val
+
+func read*[K](T: typedesc, val: openarray[K], a, b: SomeInteger): T {.inline.} =
+  cast[ptr T](cast[ptr K](unsafeAddr(val)) + a.int)[b.int]
+
+func write*[K](T: typedesc, val: openarray[K], a, b: SomeInteger, value: any) {.inline.} =
+  cast[ptr T](cast[ptr K](unsafeAddr(val)) + a.int)[b.int] = cast[T](value)
--- tests/util.nim
+++ tests/util.nim
@@ -50,3 +50,69 @@ suite "signExtend":
 
   test "negative type up":
     check(signExtend(uint16, 0x07'u8, 2) == 0xFFFF'u16)
+
+suite "read":
+  let a = [0x01'u8, 0x02'u8, 0x03'u8, 0x04'u8, 0x05'u8, 0x06'u8]
+
+  test "same type no offset":
+    check(read(uint8, a, 0, 0) == 0x01)
+
+  test "same type base offset":
+    check(read(uint8, a, 1, 0) == 0x02)
+
+  test "same type type offset":
+    check(read(uint8, a, 0, 1) == 0x02)
+
+  test "same type both offset":
+    check(read(uint8, a, 1, 1) == 0x03)
+
+  test "diff type no offset":
+    check(read(uint16, a, 0, 0) == 0x0201)
+
+  test "diff type base offset":
+    check(read(uint16, a, 1, 0) == 0x0302)
+
+  test "diff type type offset":
+    check(read(uint16, a, 0, 1) == 0x0403)
+
+  test "diff type both offset":
+    check(read(uint16, a, 1, 1) == 0x0504)
+
+
+suite "write":
+  var a: array[6, uint8]
+
+  setUp:
+    a = [0x01'u8, 0x02'u8, 0x03'u8, 0x04'u8, 0x05'u8, 0x06'u8]
+
+  test "same type no offset":
+    write(uint8, a, 0, 0, 0x10)
+    check(a == [0x10'u8, 0x02'u8, 0x03'u8, 0x04'u8, 0x05'u8, 0x06'u8])
+
+  test "same type base offset":
+    write(uint8, a, 1, 0, 0x20)
+    check(a == [0x01'u8, 0x20'u8, 0x03'u8, 0x04'u8, 0x05'u8, 0x06'u8])
+
+  test "same type type offset":
+    write(uint8, a, 0, 1, 0x20)
+    check(a == [0x01'u8, 0x20'u8, 0x03'u8, 0x04'u8, 0x05'u8, 0x06'u8])
+
+  test "same type both offset":
+    write(uint8, a, 1, 1, 0x30)
+    check(a == [0x01'u8, 0x02'u8, 0x30'u8, 0x04'u8, 0x05'u8, 0x06'u8])
+
+  test "diff type no offset":
+    write(uint16, a, 0, 0, 0x2010)
+    check(a == [0x10'u8, 0x20'u8, 0x03'u8, 0x04'u8, 0x05'u8, 0x06'u8])
+
+  test "diff type base offset":
+    write(uint16, a, 1, 0, 0x3020)
+    check(a == [0x01'u8, 0x20'u8, 0x30'u8, 0x04'u8, 0x05'u8, 0x06'u8])
+
+  test "diff type type offset":
+    write(uint16, a, 0, 1, 0x4030)
+    check(a == [0x01'u8, 0x02'u8, 0x30'u8, 0x40'u8, 0x05'u8, 0x06'u8])
+
+  test "diff type both offset":
+    write(uint16, a, 1, 1, 0x5040)
+    check(a == [0x01'u8, 0x02'u8, 0x03'u8, 0x40'u8, 0x50'u8, 0x06'u8])
