0f8f1f1dec2a6d0730026f3d906912d83666b4e9
Matthew Berry <me@mattrb.com>
2021-04-26 01:47:58 -0700

    make util funcs take naturals

--- src/gba/util.nim
+++ src/gba/util.nim
@@ -1,18 +1,18 @@
-func `<<`*[T: SomeUnsignedInt](value: T, count: SomeUnsignedInt): T
+func `<<`*[T: SomeUnsignedInt](value: T, count: Natural): T
 
 # Right shift operator supporting negative and large shift amounts
-func `>>`*[T: SomeUnsignedInt](value: T, count: SomeUnsignedInt): T =
+func `>>`*[T: SomeUnsignedInt](value: T, count: Natural): T =
   if likely(count < sizeof(T) * 8): value shr count
   else: 0
 
 # Left shift operator supporting negative and large shift amounts
-func `<<`*[T: SomeUnsignedInt](value: T, count: SomeUnsignedInt): T =
+func `<<`*[T: SomeUnsignedInt](value: T, count: Natural): T =
   if likely(count < sizeof(T) * 8): value shl count
   else: 0
 
-func bit*(value, bit: SomeUnsignedInt): bool {.inline.} = bool((value >> bit) and 1)
+func bit*(value: SomeUnsignedInt, bit: Natural): bool {.inline.} = bool((value >> bit) and 1)
 
-func signExtend*(T: typedesc, value, bit: SomeUnsignedInt): T =
+func signExtend*(T: typedesc, value: SomeUnsignedInt, bit: Natural): T =
   result = cast[T](value)
   if value.bit(bit): result = result or (high(T) shl bit)
 
