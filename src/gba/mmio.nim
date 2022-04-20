290828543f5bff6b6c6c3688938cb996c09ffa65
Matthew Berry <me@mattrb.com>
2022-04-19 21:58:05 -0700

    fill out the rest of mmio

--- src/gba/mmio.nim
+++ src/gba/mmio.nim
@@ -1,5 +1,3 @@
-import strutils
-
 import types, ppu, apu, dma, timer, serial, keypad, interrupts, regs
 
 var
@@ -21,7 +19,10 @@ proc `[]`*(mmio: MMIO, address: SomeInteger): uint8 =
   of 0x200..0x203: mmio.gba.interrupts[address]
   of 0x204..0x205: read(waitcnt, address and 1)
   of 0x208..0x209: mmio.gba.interrupts[address]
-  else: echo "Unmapped MMIO read: " & address.toHex(8); 0
+  of 0x300: 0 # postflg
+  of 0x301: 0 # haltcnt (write-only)
+  of 0x800..0x803: 0 # internal memory control
+  else: 0 # todo: open bus
 
 proc `[]=`*(mmio: MMIO, address: SomeInteger, value: uint8) =
   case address
@@ -35,4 +36,8 @@ proc `[]=`*(mmio: MMIO, address: SomeInteger, value: uint8) =
   of 0x200..0x203: mmio.gba.interrupts[address] = value
   of 0x204..0x205: write(waitcnt, value, address and 1)
   of 0x208..0x209: mmio.gba.interrupts[address] = value
-  else: echo "Unmapped MMIO write: " & address.toHex(8) & " = " & value.toHex(2)
+  of 0x300: discard # postflg
+  of 0x301:
+    if value.bit(7): mmio.gba.cpu.halted = true
+  of 0x800..0x803: discard # internal memory control
+  else: discard
