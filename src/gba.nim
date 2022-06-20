eb4cb10240a8dc07146d8591692100192c5f178d
Matthew Berry <me@mattrb.com>
2022-06-20 15:19:38 -0700

    add variable refresh rate
    allows for emu to still run at 60hz when iOS is in battery saving mode

--- src/gba.nim
+++ src/gba.nim
@@ -1,5 +1,6 @@
 import os
 import sdl2
+import times
 
 import gba/[types, display, scheduler, bus, cpu, ppu, apu, dma, timer, serial, keypad, interrupts, util]
 
@@ -17,17 +18,31 @@ proc newGBA(bios, rom: string): GBA =
   result.keypad = newKeypad(result)
   result.interrupts = newInterrupts(result)
 
-var cycleCount = 0
-const cyclesPerFrame = 280896
+when defined(emscripten):
+  type em_arg_callback_func = proc(data: pointer) {.cdecl.}
+  proc emscripten_set_main_loop(fun: proc() {.cdecl.}, fps, simulate_infinite_loop: cint) {.header: "<emscripten.h>".}
+  proc emscripten_set_main_loop_arg(fun: em_arg_callback_func, arg: pointer, fps, simulate_infinite_loop: cint) {.header: "<emscripten.h>".}
+  proc emscripten_cancel_main_loop() {.header: "<emscripten.h>".}
+  proc emscripten_get_now(): float {.header: "<emscripten.h>".}
 
-# Run long enough to produce the next frame, then return.
-proc runFrame(gba: GBA) =
-  while likely(cycleCount < cyclesPerFrame):
+var cycleCount = 0
+var time: float = 0
+const cyclesPerSecond = 16777216
+
+# Return real-time number of seconds as a relative value.
+proc getTime(): float =
+  when defined(emscripten): emscripten_get_now() / 1000
+  else: epochTime()
+
+# Run for given number of cycles adjusted for extra cycles in previous frame.
+proc runCycles(gba: GBA, cycles: int) =
+  while likely(cycleCount < cycles):
     let accessCycles = gba.cpu.tick()
     gba.scheduler.tick(accessCycles)
     cycleCount += accessCycles
-  cycleCount -= cyclesPerFrame
+  cycleCount -= cycles
 
+# Handle and pass along key input.
 proc checkKeyInput(gba: GBA) =
   var event = sdl2.defaultEvent
   while pollEvent(event):
@@ -36,26 +51,30 @@ proc checkKeyInput(gba: GBA) =
     of KeyDown, KeyUp: gba.keypad.keyEvent(cast[KeyboardEventObj](event))
     else: discard
 
+# Run roughly one frame from the emulator, regardless of host device's
+# refresh rate. After running the emulator, handle any input.
 proc loop(gba: GBA) {.cdecl.} =
-  runFrame(gba)
+  let curTime = getTime()
+  let elapsed = curTime - time
+  time = curTime
+  let cyclesToRun = int(elapsed * cyclesPerSecond)
+  runCycles(gba, cyclesToRun)
   checkKeyInput(gba)
 
 discard sdl2.init(INIT_EVERYTHING)
 
 when defined(emscripten):
-  type em_arg_callback_func = proc(data: pointer) {.cdecl.}
-  proc emscripten_set_main_loop(fun: proc() {.cdecl.}, fps, simulate_infinite_loop: cint) {.header: "<emscripten.h>".}
-  proc emscripten_set_main_loop_arg(fun: em_arg_callback_func, arg: pointer, fps, simulate_infinite_loop: cint) {.header: "<emscripten.h>".}
-  proc emscripten_cancel_main_loop() {.header: "<emscripten.h>".}
   proc initFromEmscripten() {.exportc.} =
     emscripten_cancel_main_loop() # cancel the main loop if it's running
     cycleCount = 0 # reset the cycle count
     # todo: memory is leaked when each new rom is loaded
     var gba = newGBA("bios.bin", "rom.gba")
+    time = getTime()
     emscripten_set_main_loop_arg(cast[em_arg_callback_func](loop), cast[pointer](gba), -1, 1)
 else:
   if paramCount() != 2: quit "Run with ./gba /path/to/bios /path/to/rom"
   var gba = newGBA(paramStr(1), paramStr(2))
+  time = getTime()
   while true:
     loop(gba)
     sleepUntilEndOfFrame()
